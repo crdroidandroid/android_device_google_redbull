ca78765952a71098fc5f704ec3428f83866a93dc
Michael Bestas <mkbestas@lineageos.org>
2025-04-28 04:49:14 +0300

    redbull: Migrate gralloc_handle_has_reserved_size to soong_config_set
    Change-Id: I3aa081f081e41ba108ff91227409baad7f54d49c

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -30,9 +30,6 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
 DEVICE_MANIFEST_FILE += device/google/redbull/lineage_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 
-# Needed for camera
-TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
-
 # Partitions
 AB_OTA_PARTITIONS += \
     vendor
